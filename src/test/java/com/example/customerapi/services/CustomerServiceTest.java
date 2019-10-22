37a21ad5ea5cdf77238a2ec5c07c955237095e3b
DivineNinth <divineninth@outlook.com>
2019-10-22 15:35:21 -0500

    changed service test class and app.prop

--- src/test/java/com/example/customerapi/services/CustomerServiceTest.java
+++ src/test/java/com/example/customerapi/services/CustomerServiceTest.java
@@ -1,19 +1,16 @@
 package com.example.customerapi.services;
 
-
-import org.junit.runner.RunWith;
 import com.example.customerapi.entities.Customer;
 import com.example.customerapi.repo.CustomerRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
