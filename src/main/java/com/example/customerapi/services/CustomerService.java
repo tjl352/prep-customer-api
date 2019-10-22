4e043d61fb944a31b2aae7855fee294981487bfd
DivineNinth <divineninth@outlook.com>
2019-10-22 15:15:14 -0500

    Starting Customer Service Test

---
+++ src/main/java/com/example/customerapi/services/CustomerService.java
@@ -0,0 +1,15 @@
+package com.example.customerapi.services;
+
+import com.example.customerapi.entities.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    public List<Customer> findAll() {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/example/customerapi/services/CustomerServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.customerapi.services;
+
+
+import org.junit.runner.RunWith;
+import com.example.customerapi.entities.Customer;
+import com.example.customerapi.repo.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CustomerServiceTest {
+
+    @Autowired
+    CustomerRepository repo;
+
+    @Autowired
+    CustomerService service;
+
+    @Test
+    public void getAllCustomers() {
+        /*
+            Get "all" customers from database
+         */
+        //Setup
+        List<Customer> customers = new ArrayList();
+
+        Customer c = new Customer();
+
+        c.setFirstName("John");
+        customers.add(c);
+
+        c.setFirstName("Bill");
+        customers.add(c);
+
+        c.setFirstName("Sara");
+        customers.add(c);
+
+        //Exercise
+        repo.saveAll(customers);
+        List<Customer> actual = null;
+        actual = service.findAll();
+
+        //Assert
+        assertNotNull(actual);
+        assertTrue(actual.size() == customers.size());
+    }
+
+
+
+
+
+}
